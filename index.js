7a8b718ff95d3062c1f007440f0aba1ea7238891
Shubham mehra <shubhamkumar3169@gmail.com>
2025-01-09 14:59:45 +0530

    first commit

--- index.js
+++ index.js
@@ -1,52 +1,43 @@
 require('dotenv').config();
-const express = require ("express");
+const express = require("express");
 const mongoose = require("mongoose");
-const app = express()
-const cors =require("cors")
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const bodyParser=require('body-parser')
-// const cookieParser = require('cookie-parser')
+const bodyParser = require("body-parser");
 
+const app = express();
 
+// Middleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-
-app.use(cookieParser())
-app.use(bodyParser.json())
-
-
-app.use(cors())
-app.use (express.json())
-app.use(express.urlencoded({ extended: true }))
-
-
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log("Database connected!");
-
-}
-
-app.get("/",(req,res)=>{
-    res.send("API is working!")
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-
-// importing the usermodel//
-
-const UserRoute =require("./Route/UserRoute")
-app.use("/api",UserRoute)
-
-const BookRoute =require("./Route/BookRoute")
-app.use("/api",BookRoute)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+  console.log("Mongo URI:", process.env.MONGO_URI);
 
 
+// Root Route
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
 
+// Routes
+const UserRoute = require("./Route/UserRoute");
+app.use("/api/users", UserRoute);
 
+const BookRoute = require("./Route/BookRoute");
+app.use("/api/books", BookRoute);
+
+// Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log(`server wroking on the port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server working on port ${PORT}`);
+});
