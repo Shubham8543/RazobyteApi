94cbc78434898faaec922bfd5679a0d8ed78a324
Shubham mehra <shubhamkumar3169@gmail.com>
2025-01-09 15:07:46 +0530

    first commit

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ app.get("/", (req, res) => {
 
 // Routes
 const UserRoute = require("./Route/UserRoute");
-app.use("/api/users", UserRoute);
+app.use("/api", UserRoute);
 
 const BookRoute = require("./Route/BookRoute");
-app.use("/api/books", BookRoute);
+app.use("/api", BookRoute);
 
 // Start Server
 const PORT = process.env.PORT || 3000;
